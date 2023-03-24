576487de8e69130e00caa36dfe2a20d39a62e20a
skval55 <skvaladez@gmail.com>
2023-03-23 19:03:34 -0600

    added pages to see,add,edit animals

--- app.py
+++ app.py
@@ -1,12 +1,70 @@
-from flask import Flask, render_template, flash, redirect, render_template
+from flask import Flask, render_template, flash, redirect
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, Animal
+
+from forms import AddAnimalForm
+from forms import EditAnimalForm
+
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "oh-so-secret"
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///adoption_agency"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
 connect_db(app)
+
+@app.route('/')
+def homepage():
+    """home page"""
+
+    animals = Animal.query.all()
+
+    return render_template("home.html", animals=animals)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_animal():
+    """add animal for and post animal"""
+
+    form = AddAnimalForm()
+    for x in form:
+
+        print(x.type)
+    print("*****************")
+
+    if form.validate_on_submit():
+        name = form.name.data
+        species = form.species.data
+        photo_url = form.photo_url.data
+        if photo_url == "":
+            photo_url = 'https://i.pinimg.com/originals/8c/0c/ea/8c0ceac06ec6bf6e3b7065e4346f30a1.gif'
+        age = form.age.data
+        notes = form.notes.data
+        db.session.add(Animal(name=name, species=species, photo_url=photo_url, age=age, notes=notes))
+        db.session.commit()
+
+        return redirect('/')
+    else:
+        return render_template("add-animal.html", form=form)
+
+
+@app.route('/<int:animal_id>', methods=['GET', 'POST'])
+def animal_page(animal_id):
+    """shows animals detail and gives option to edit"""
+
+    animal = Animal.query.get_or_404(animal_id)
+    form = EditAnimalForm(obj = animal)
+
+    if form.validate_on_submit():
+        animal.photo_url = form.photo_url.data
+        if animal.photo_url == "":
+            animal.photo_url = 'https://i.pinimg.com/originals/8c/0c/ea/8c0ceac06ec6bf6e3b7065e4346f30a1.gif'
+        animal.notes = form.notes.data
+        animal.available = form.available.data
+        db.session.commit()
+        return redirect(f'/{animal.id}')
+    else:
+        return render_template('animal-page.html', animal = animal, form=form)
--- forms.py
+++ forms.py
@@ -1,4 +1,24 @@
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, FloatField
-from wtforms.validators import InputRequired, Optional, Email
+from wtforms import StringField, FloatField, IntegerField, BooleanField
+from wtforms.validators import InputRequired, Optional, AnyOf, URL, NumberRange
+
+class AddAnimalForm(FlaskForm):
+    """form for adding an animal"""
+
+    name = StringField("Animal name",
+                       validators=[InputRequired()])
+    species = StringField("Species",
+                       validators=[InputRequired(), AnyOf(["cat", 'dog', 'porcupine'])])
+    photo_url = StringField("Photo URL", validators=[URL(), Optional()])
+    age = IntegerField('Age', validators=[NumberRange(min=0,max=30,message='age out of range')])
+    notes = StringField("Notes")
+
+class EditAnimalForm(FlaskForm):
+    """form for editing an animal"""
+
+    photo_url = StringField("Photo URL", validators=[URL(), Optional()])
+    age = IntegerField('Age', validators=[NumberRange(min=0,max=30,message='age out of range')])
+    notes = StringField("Notes")
+    available = BooleanField('Available', default=False)
+
